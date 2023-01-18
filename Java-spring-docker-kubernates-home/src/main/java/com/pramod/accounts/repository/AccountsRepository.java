00323622331e8aadbf2b8c187280804484eb570a
Officialravi888 <youremail@yourdomain.com>
2023-01-18 11:29:48 +0530

    ZipFileProject

---
+++ Java-spring-docker-kubernates-home/src/main/java/com/pramod/accounts/repository/AccountsRepository.java
@@ -0,0 +1,10 @@
+package com.pramod.accounts.repository;
+
+import com.pramod.accounts.model.Accounts;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccountsRepository extends CrudRepository<Accounts, Long> {
+        Accounts findByCustomerId(int customerId);
+        }
